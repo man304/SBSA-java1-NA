6d0db8667b60996fe0aeef861caa28c0e0aa9059
unknown <nurumi304@gmail.com>
2020-08-03 18:50:14 +0900

    2nd Commit

--- src/Postclass.java
+++ src/Postclass.java
@@ -3,34 +3,45 @@ import java.util.Scanner;
 public class Postclass {
 
 	public static void main(String[] args) {
+	
 		
 		Scanner S = new Scanner(System.in);
-		int postnumber = 0;
-		String[] P = new String[postnumber];//글목록
-	
+		String title[] = new String[10]; //제목
+		String body[] = new String[10];  //본문
+		int post_NO = 0;
+		//
 		while(true) {
 			System.out.println("명령어를 입력해주세요");
 			String txt = S.nextLine();
-		
-			if(txt.equals("exit")) {
+			
+			//종료
+			if(txt.equals("exit")) { 
 				System.out.println("시스템 종료");
 				break;
 			}
-			else if (txt.equals("help")) {
+			//명령어안내
+			else if (txt.equals("help")) { 
 				System.out.println("==== 명령어 목록 ====");
 				System.out.println("exit - 시스템 종료");
 				System.out.println("add - 게시물 정보를 저장");
 				System.out.println("list - 게시물 목록 출력");
 				System.out.println("=====================");
 			}
-			else if (txt.equals("add")) {
-				postnumber += 1;
-				String Save = S.nextLine();
-				P[postnumber] = Save;
+			//글작성
+			else if (txt.equals("add")) { 
+				System.out.println("제목을 입력해주세요");
+				title[post_NO] = S.nextLine(); //제목
+				System.out.println("내용을 입력해주세요");
+				body[post_NO] = S.nextLine();  //본문
+				post_NO ++;
 			}
+			//글목록불러오기
 			else if (txt.equals("list")) {
-				for(int i= 0; i < P.length; i++) {
-				System.out.println(P[i]);
+				for(int i = 0; i < post_NO; i++) {
+					
+					System.out.println("제목 : " + title[post_NO]);
+					System.out.println("내용 : " + body[post_NO]);
+					System.out.println(" ");
 				}
 			}
 			else {
@@ -39,6 +50,6 @@ public class Postclass {
 		
 		}
 
+		
 	}
-
 }
